12f36d89277288100cd74820cbf99880a03d7da6
PieroSimonet <PieroSimonet@users.noreply.github.com>
2015-11-05 15:59:27 +0100

    Update coda_statica.c

--- coda_statica.c
+++ coda_statica.c
@@ -1,1 +1,20 @@
-// init
+//Gruppo Marini_Simonetto_Biasia...
+void shift(int V[],int max,int *l){
+  for(int i=0;i<max-1,i++)
+    V[i]=V[i+1];
+  l--;
+}
+int dequeue(int V[],int max,int *l){
+  int tmp=V[0];
+  if(l>0){
+    shift(V,max,l);
+    return tmp;
+  }
+  return 0;
+}
+int enqueue(int V[],int max,int dato,int* l){
+  if(l==max)
+    return 1;
+  V[l]=dato;
+  return 0;
+}
