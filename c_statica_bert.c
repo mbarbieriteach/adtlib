78555a0aa70e96a9977febbb872d6e7b0a6538bd
Roberto Zotta <raykoto98@gmail.com>
2015-11-07 11:44:36 +0100

    c_statica_bert_senzalinutile.c
    La "int l" nello shift non serviva :3

--- c_statica_bert.c
+++ c_statica_bert.c
@@ -1,17 +1,17 @@
-void shift(int[] v, int max, int* l)
+void shift(int[] v, int max)
 {
   for(int i=0; i < max; i++)
     V[i] = v[i+1];
 }
 
-int dequeue(int[] v, int max, int* l)
+int dequeue(int[] v, int max, int l)
 {
   int tmp = v[0];
-  shift(v, max, l);
+  shift(v, max);
   return tmp;
 }
 
-int enqueue(int[] v, int max, int dato, int* l)
+int enqueue(int[] v, int max, int dato, int l)
 {
   if(l == max)
     return 0;
